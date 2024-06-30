209f7a78a13374cee178b1473f7692fe53e62c7a
matyasgodula <matyasgodula@gmail.com>
2024-06-30 20:21:37 +0200

    Initial commit

---
+++ src/main/java/cz/cvut/fel/pjv/Lab02.java
@@ -0,0 +1,13 @@
+/*
+ * File name: Lab06.java
+ * Date:      2014/08/26 21:39
+ * Author:    @author
+ */
+
+package cz.cvut.fel.pjv;
+
+public class Lab02 {
+
+   public void start(String[] args) {
+   }
+}
