ec2761ede8f14e78f435616e91f579c0bd90992b
matyasgodula <matyasgodula@gmail.com>
2024-06-30 23:42:01 +0200

    changed a few things

--- src/main/java/cz/cvut/fel/pjv/Lab02.java
+++ src/main/java/cz/cvut/fel/pjv/Lab02.java
@@ -6,8 +6,54 @@
 
 package cz.cvut.fel.pjv;
 
-public class Lab02 {
+import java.util.Scanner;
+import java.util.ArrayList;
 
-   public void start(String[] args) {
-   }
+public class Lab02 {
+    static int counter = 0;
+    static boolean finished_reading = false;
+
+    public void start(String[] args) {
+        homework();
+    }
+
+    public void homework() {
+        TextIO textIO = new TextIO();
+        while (true) {
+            ArrayList<Double> current_list = read_file(textIO);
+            int list_size = current_list.size();
+            System.out.println(current_list);
+            if (!finished_reading) {
+                // calculate and print but dont break
+            } else if (finished_reading && list_size > 1) {
+                // calculate and print then break
+                break;
+            } else { 
+                break; 
+            }
+        }
+    }
+
+    public ArrayList<Double> read_file(TextIO textIO) {
+        int inner_counter = 0;
+        String current_line;
+        ArrayList<Double> number_list = new ArrayList<Double>();
+        while (inner_counter < 10) {
+            current_line = textIO.getLine();
+            if (current_line == "") {
+                finished_reading = true;
+                break;
+            }
+            counter += 1;
+            //System.out.println(current_line);
+            if (textIO.isDouble(current_line)) {
+                inner_counter += 1;
+                double current_number = Double.parseDouble(current_line);
+                number_list.add(current_number);
+            } else {
+                System.out.printf("A number has not been parsed from line %d\n", counter);
+            }
+        }
+        return number_list;
+    }
 }
